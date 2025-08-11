3a8fd85dc785c00e0ca667dc685b450aaa93c3ef
prithviraj-jadeja <40898178+prithviraj-jadeja@users.noreply.github.com>
2025-08-11 21:17:34 +1000

    New instance ip updated in axiosconfig.js

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.64.179.101:5001', // live
+  baseURL: 'http://54.253.231.25:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
