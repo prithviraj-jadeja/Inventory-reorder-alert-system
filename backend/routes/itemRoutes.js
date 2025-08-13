2ac4c83d283dbfda096464d55b519cb5399ad6c6
prithviraj-jadeja <40898178+prithviraj-jadeja@users.noreply.github.com>
2025-08-14 04:04:55 +1000

    IRA-58 landing page and forms update

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const { getItems, addItem, updateItem, deleteItem } = require('../controllers/alertController');
 const { protect } = require('../middleware/authMiddleware');
-const { getItems, addItem } = require('../controllers/alertController');
 const router = express.Router();
 
 router.route('/').get(protect, getItems).post(protect, addItem);
