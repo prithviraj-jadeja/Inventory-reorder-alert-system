05117042e1fa4af31bcba2d3ba8b36ce4a9f6f0b
prithviraj-jadeja <40898178+prithviraj-jadeja@users.noreply.github.com>
2025-08-25 16:38:47 +1000

    Test

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.65.233.83:5001', // live
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://52.65.233.83:5001', // live
   headers: { 'Content-Type': 'application/json'},
 });
 
