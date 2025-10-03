7cb90f0ad0d382a3cf4b724a4fbe16aef9e83250
prithviraj-jadeja <40898178+prithviraj-jadeja@users.noreply.github.com>
2025-10-03 19:09:25 +1000

    Proxy design pattern implemented in authROutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,14 @@
-
 const express = require('express');
 const { registerUser, loginUser, updateUserProfile, getProfile } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
+const Proxyhandler = require('./Proxy');
+
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/profile', protect, getProfile);
-router.put('/profile', protect, updateUserProfile);
+router.post('/register', (req, res, next) => new Proxyhandler(registerUser).handle(req, res, next));
+router.post('/login', (req, res, next) => new Proxyhandler(loginUser).handle(req, res, next));
+router.get('/profile', (req, res, next) => new Proxyhandler(getProfile, [protect]).handle(req, res, next));
+router.put('/profile', (req, res, next) => new Proxyhandler(updateUserProfile, [protect]).handle(req, res, next));
+
 
 module.exports = router;
